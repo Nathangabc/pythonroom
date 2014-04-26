124b2e8651b87b3d7ca74c442dbf543c64f1400f
Nathangabc <chrtran@yahoo.com>
2014-04-26 13:34:47 -0700

    pythonroom update

--- turtledraw.py
+++ turtledraw.py
@@ -1,17 +1,11 @@
 # author: Nathangabc
 import turtle
 t= turtle.Turtle()
-length = 100
-angle = 60
-t.forward(length)
-t.left(angle)
-t.forward(length)
-t.left(angle)
-t.forward(length)
-t.left(angle)
-t.forward(length)
-t.left(angle)
-t.forward(length)
-t.left(angle)
-t.forward(length)
-t.left(angle)
+length = 1
+angle = 1
+numbers = [ 1, 2, 3, 4, 5 ]
+count = 0
+while count < 360:
+	t.forward(length)
+	t.left(angle)
+	count = count + 1
