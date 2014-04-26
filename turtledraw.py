48b538ad5577c3e2778681947e666353254b6211
Nathangabc <chrtran@yahoo.com>
2014-04-26 13:18:36 -0700

    pythonroom update

--- turtledraw.py
+++ turtledraw.py
@@ -1,1 +1,15 @@
 # author: Nathangabc
+import turtle
+t= turtle.Turtle()
+length = 100
+angle = 72
+t.forward(length)
+t.left(angle)
+t.forward(length)
+t.left(angle)
+t.forward(length)
+t.left(angle)
+t.forward(length)
+t.left(angle)
+t.forward(length)
+t.left(angle)
