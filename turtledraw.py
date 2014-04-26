3f15f23659ad0e59229eb16ead4918a5953059ec
Nathangabc <chrtran@yahoo.com>
2014-04-26 14:06:07 -0700

    pythonroom update

--- turtledraw.py
+++ turtledraw.py
@@ -1,13 +1,15 @@
 # author: Nathangabc
 import turtle
 t= turtle.Turtle()
-length = 100
-angle = 361
-numbers = range(10, 100)
+length = 200
+angle = 250
+numbers = range(0, 100)
 #colors = ["yellow", "blue", "green", "red", "magenta", "orange" ]
 for number in numbers:
 	#t.color(color)
 	t.forward(length)
 	t.left(angle)
+	
+	
 	
 	
